4afd818338dda07c5e4a4e956f591f25ac399101
rihee1214 <rihee1214@gmail.com>
2021-05-10 15:30:06 +0900

    2

---
+++ SpringBoot_Mybatis_Thymeleaf/src/main/java/com/iu/s1/SpringBootMybatisThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.iu.s1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMybatisThymeleafApplication.class, args);
+	}
+
+}
